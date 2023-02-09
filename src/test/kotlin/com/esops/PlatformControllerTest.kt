c0a07618949d570c7b3ac69edb01cce792a23166
MaheshAravindV <maheshv@sahaj.ai>
2023-02-09 21:29:50 +0530

    Remove duplicate tests, fix tests

--- src/test/kotlin/com/esops/PlatformControllerTest.kt
+++ src/test/kotlin/com/esops/PlatformControllerTest.kt
@@ -1,10 +1,7 @@
 package com.esops
 
-import io.micronaut.http.HttpStatus
-import io.micronaut.http.client.HttpClient
 import io.micronaut.runtime.server.EmbeddedServer
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 @MicronautTest
