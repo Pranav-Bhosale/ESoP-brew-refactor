d4b50346adc5ecf8842a7f14b5ab647621a6279a
MaheshAravindV <maheshv@sahaj.ai>
2023-02-09 18:46:19 +0530

    Allow access to active order lists

--- src/main/kotlin/com/esops/service/PlatformService.kt
+++ src/main/kotlin/com/esops/service/PlatformService.kt
@@ -1,11 +1,15 @@
 package com.esops.service
 
+import com.esops.entity.EsopType
+import com.esops.entity.Order
+import com.esops.entity.OrderType
 import com.esops.entity.Platform
 import com.esops.repository.ActiveBuyOrders
 import com.esops.repository.ActiveNonPerformanceSellOrders
 import com.esops.repository.ActivePerformanceSellOrders
 import jakarta.inject.Inject
 import jakarta.inject.Singleton
+import org.aspectj.weaver.ast.Or
 import java.math.BigInteger
 
 @Inject
@@ -38,4 +42,25 @@ class PlatformService {
             ?: return
         orderService.executeOrder(buyOrder, sellOrder)
     }
+
+    fun addOrderToQueue(order: Order){
+        when(order.type){
+            OrderType.BUY -> addBuyOrder(order)
+            OrderType.SELL -> addSellOrder(order)
+        }
+    }
+
+    private fun addSellOrder(order: Order) {
+        when(order.esopType){
+            EsopType.PERFORMANCE -> addPerformanceSellOrder(order)
+            EsopType.NON_PERFORMANCE -> addNonPerformanceSellOrder(order)
+        }
+    }
+
+    private fun addPerformanceSellOrder(order: Order) = activePerformanceSellOrders.addOrder(order)
+
+
+    private fun addNonPerformanceSellOrder(order: Order) = activeNonPerformanceSellOrders.addOrder(order)
+
+    private fun addBuyOrder(order: Order) = activeBuyOrders.addOrder(order)
 }
