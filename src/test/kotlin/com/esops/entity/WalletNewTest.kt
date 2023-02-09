40ce52d4fe33c0f12e2b9856319242134da5f189
MaheshAravindV <maheshv@sahaj.ai>
2023-02-09 16:00:04 +0530

    Add WalletNew class to replace Wallet in the future

---
+++ src/test/kotlin/com/esops/entity/WalletNewTest.kt
@@ -0,0 +1,68 @@
+package com.esops.entity
+
+import com.esops.exception.WalletException
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.math.BigInteger
+
+class WalletNewTest {
+    @Test
+    fun `should be able to add money`() {
+        val wallet = WalletNew()
+        val amount = BigInteger.TEN
+
+        assertDoesNotThrow { wallet.add(amount) }
+    }
+
+    @Test
+    fun `should not be able to add negative amount`() {
+        val wallet = WalletNew()
+        val amount = BigInteger("-10")
+
+        val exception = assertThrows(WalletException::class.java) { wallet.add(amount) }
+        assertEquals("Amount to be added has to be positive.", exception.errorList[0])
+    }
+
+    @Test
+    fun `should not be able to add more than the max limit`() {
+        val wallet = WalletNew()
+        val amount = BigInteger("100000000000000000001")
+
+        val exception = assertThrows(WalletException::class.java) { wallet.add(amount) }
+        assertEquals("Invalid amount. Wallet maximum threshold will be exceeded.", exception.errorList[0])
+    }
+
+    @Test
+    fun `should not be able to lock more money than wallet has free`() {
+        val wallet = WalletNew()
+        val amountToAdd = BigInteger.TWO
+        val amountToLock = BigInteger.TEN
+        wallet.add(amountToAdd)
+
+        val exception = assertThrows(WalletException::class.java) { wallet.lockMoney(amountToLock) }
+        assertEquals("Insufficient funds.", exception.errorList[0])
+    }
+
+    @Test
+    fun `should not be able to remove more money than wallet has locked`() {
+        val wallet = WalletNew()
+        val amountToLock = BigInteger.TWO
+        val amountToRemove = BigInteger.TEN
+        wallet.add(amountToLock)
+        wallet.lockMoney(amountToLock)
+
+        val exception = assertThrows(WalletException::class.java) { wallet.removeLockedMoney(amountToRemove) }
+        assertEquals("Invalid amount. Wallet amount has to be positive.", exception.errorList[0])
+    }
+
+    @Test
+    fun `should be able to remove money`() {
+        val wallet = WalletNew()
+        val amount = BigInteger.ONE
+
+        wallet.add(amount)
+        wallet.lockMoney(amount)
+
+        assertDoesNotThrow { wallet.removeLockedMoney(amount) }
+    }
+}
