d3e107da9c19ebe21fa3c8f02f2bf0082e5c880c
MaheshAravindV <maheshv@sahaj.ai>
2023-02-09 13:20:40 +0530

    Add execute orders functionality to PlatformService

--- src/main/kotlin/com/esops/service/PlatformService.kt
+++ src/main/kotlin/com/esops/service/PlatformService.kt
@@ -1,8 +1,24 @@
 package com.esops.service
 
 import com.esops.entity.Platform
+import com.esops.repository.ActiveBuyOrders
+import com.esops.repository.ActiveNonPerformanceSellOrders
+import com.esops.repository.ActivePerformanceSellOrders
+import jakarta.inject.Inject
 import jakarta.inject.Singleton
 import java.math.BigInteger
+
+@Inject
+lateinit var activeBuyOrders: ActiveBuyOrders
+
+@Inject
+lateinit var activePerformanceSellOrders: ActivePerformanceSellOrders
+
+@Inject
+lateinit var activeNonPerformanceSellOrders: ActiveNonPerformanceSellOrders
+
+@Inject
+lateinit var orderService: OrderService
 
 @Singleton
 class PlatformService {
@@ -13,5 +29,13 @@ class PlatformService {
 
     fun addPlatformFees(fee: BigInteger) {
         platform.add(fee)
+    }
+
+    fun executeOrders() {
+        val buyOrder = activeBuyOrders.getBestBuyOrder() ?: return
+        val sellOrder =
+            activePerformanceSellOrders.getBestSellOrder(buyOrder) ?: activeNonPerformanceSellOrders.getBestSellOrder()
+            ?: return
+        orderService.executeOrder(buyOrder, sellOrder)
     }
 }
